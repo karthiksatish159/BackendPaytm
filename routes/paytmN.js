6a3f74080d47ac1db34d225d4d3f9850bc7ef9d2
karthiksatish159 <74001872+karthiksatish159@users.noreply.github.com>
2022-06-21 16:46:38 +0530

    eight

--- routes/paytmN.js
+++ routes/paytmN.js
@@ -7,11 +7,6 @@ const config = require("../Paytm/config");
 const parseUrl = express.urlencoded({ extended: false });
 const parseJson = express.json({ extended: false });
 const payment=require('../models/Payments');
-// var Amount;
-// var email="";
-// var phone="";
-// var uname="";
-// var coins_;
 var paymentDetails={
   amount:"",
   customerId:"",
@@ -20,25 +15,15 @@ var paymentDetails={
 };
 router.post('/paynow',[parseUrl, parseJson],(req,res)=>
 {
-    var m=parseInt(req.body.amount)*10;
-    var k="";
-    k=m.toString();
-    //console.log(k);
-     paymentDetails.amount=k;
-     paymentDetails.customerId=req.body.name;
-     paymentDetails.customerEmail=req.body.email;
-     paymentDetails.customerPhone=req.body.phone;
-    console.log(paymentDetails);
-  
-    //  uname=req.body.name;
-    //  Amount=parseInt(req.body.amount)*10;
-    //  email=req.body.email;
-    //  phone=req.body.phone;
-      //amount means coins and above i am making the conversion that coins into amount by multiplying by 10
-    //  coins_=parseInt(req.body.amount);
-
-
-     if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.customerEmail || !paymentDetails.customerPhone) {
+    let conversion=parseInt(req.body.amount)*10;
+    let value="";
+    value=conversion.toString();
+    //Dont confuse the name of attribute is amount but it will take the 
+    paymentDetails.amount=value;
+    paymentDetails.customerId=req.body.name;
+    paymentDetails.customerEmail=req.body.email;
+    paymentDetails.customerPhone=req.body.phone;
+    if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.customerEmail || !paymentDetails.customerPhone) {
          res.status(400).send('Payment failed')
      } else {
          var params = {};
@@ -148,7 +133,10 @@ router.post("/callback", (req, res) => {
                     })
                     newTrans.save().then((newObj)=>
                     {
-                        var coins_=paymentDetails.amount;
+                        let conversion=parseInt(paymentDetails.amount);
+                        conversion=conversion/10;
+                        let value=conversion.toString();
+                        var coins_=value;
                         if(newObj)
                         {
                             if(typeof coins_==='undefined')
@@ -157,7 +145,7 @@ router.post("/callback", (req, res) => {
                             }
                             else
                             {
-                              res.redirect(`/coins/addCoins/${coins_}/${newObj.email}`);
+                              res.redirect(`/coins/addCoins/${coins_}/${paymentDetails.customerEmail}`);
                             }
                         }
                         else
